b2d6b570bc5331f73ada75887496eaea75ff2a71
tylerstarr01 <tylerstarr@live.com>
2021-01-07 20:54:54 -0700

    Created some camera and image saving functionality. Tried to make it in its own class but couldn't, so its in MainActivity.

--- app/src/main/java/com/example/chat_app/MainActivity.java
+++ app/src/main/java/com/example/chat_app/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.chat_app;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,10 +26,20 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    // List of permissions needed for the program, add new ones as needed
+    //See checkPermissions method below
+    private final String[] PERMISSIONS = {Manifest.permission.CAMERA};
+    private final int[] PERMISSION_CODES = {1};
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -94,6 +114,79 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+
     }
+
+
+    private final int CAMERA_CODE = 1;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Camera Permission Granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_CODE);
+            } else {
+                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
+
+    private void userSelect() throws Exception{
+        final String[] userOptions = {"Use Camera", "Choose From Phone", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Upload Profile Photo");
+        builder.setItems(userOptions, new DialogInterface.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(PERMISSIONS, PERMISSION_CODES[0]);
+                } else {
+                    if (userOptions[i].equals("Use Camera")) {
+                        // Open Camera
+                        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivity(cameraIntent);
+
+                    } else if (userOptions[i].equals("Choose From Phone")) {
+                        Intent picker = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivity(picker);
+
+                    } else {
+                        dialogInterface.dismiss();
+                    }
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            ContextWrapper cw = new ContextWrapper(getApplicationContext());
+
+        }
+
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkPermissions () {
+        for (int i = 0; i < PERMISSIONS.length; i++) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(PERMISSIONS, PERMISSION_CODES[i]);
+            }
+        }
+    }
+
+
 
 }
